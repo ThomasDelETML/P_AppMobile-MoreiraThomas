b444728de4d6c25c5e75e54c11525a5ec24900d1
ThomasDelETML <po51oro@eduvaud.ch>
2025-04-11 15:38:32 +0200

    feat(Lecture) : Page de lecture fait
    [30][DONE]

--- code/MauiApp1/Lire.xaml.cs
+++ code/MauiApp1/Lire.xaml.cs
@@ -11,4 +11,9 @@ public partial class Lire : ContentPage
     {
         await Navigation.PushAsync(new About());
     }
+
+    private async void OnPageButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Lire());
+    }
 }
